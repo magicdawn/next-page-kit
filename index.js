19002706df5268e4fe957e162e95f4a4f21bf023
magicdawn@qq.com <magicdawn@qq.com>
2016-06-25 12:11:04 +0800

    code: get semi back

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-'use strict'
+'use strict';
 
 /**
  * module dependencies
  */
 
-const fs = require('fs-extra')
-const path = require('path')
-const URL = require('url')
-const co = require('co')
-const cheerio = require('cheerio')
-const request = require('superagent')
-require('superagent-charset')(request)
-const _ = require('lodash')
-const debug = require('debug')('next-page:index')
-const isPromise = require('is-promise')
+const fs = require('fs-extra');
+const path = require('path');
+const URL = require('url');
+const co = require('co');
+const cheerio = require('cheerio');
+const request = require('superagent');
+require('superagent-charset')(request);
+const _ = require('lodash');
+const debug = require('debug')('next-page:index');
+const isPromise = require('is-promise');
 
 const NextPage = exports = module.exports = class NextPage {
   constructor(options) {
-    _.assign(this, options)
+    _.assign(this, options);
 
     if (!options || !options.action || !options.hasNext || !options.getNext) {
-      throw new TypeError('action/hasNext/getNext can not be empty')
+      throw new TypeError('action/hasNext/getNext can not be empty');
     }
   }
 
@@ -32,46 +32,46 @@ const NextPage = exports = module.exports = class NextPage {
 
   // 下页 url
   getNext($) {}
-}
+};
 
 NextPage.prototype.run = co.wrap(function*(url, options) {
-  let html, $, index, p
-  options = options || {}
-  const enc = options.enc
-  const limit = options.limit || Infinity // 无限制页数
-  if (!url) throw new Error('url can not be empty')
+  let html, $, index, p;
+  options = options || {};
+  const enc = options.enc;
+  const limit = options.limit || Infinity; // 无限制页数
+  if (!url) throw new Error('url can not be empty');
 
   // init
   if (this.init && isPromise(p = this.init())) {
-    yield p
+    yield p;
   }
 
-  index = 0
-  debug('process: index = %s, url = %s', index, url)
+  index = 0;
+  debug('process: index = %s, url = %s', index, url);
   html = yield request
     .get(url)
     .charset(enc)
-    .then(res => res.text)
-  $ = cheerio.load(html)
-  $._currentUrl = url
-  this.action($)
+    .then(res => res.text);
+  $ = cheerio.load(html);
+  $._currentUrl = url;
+  this.action($);
 
   while (this.hasNext($) && ++index < limit) {
-    const rel = this.getNext($)
-    url = URL.resolve(url, rel)
+    const rel = this.getNext($);
+    url = URL.resolve(url, rel);
 
-    debug('process: index = %s, url = %s', index, url)
+    debug('process: index = %s, url = %s', index, url);
     html = yield request
       .get(url)
       .charset(enc)
-      .then(res => res.text)
-    $ = cheerio.load(html)
-    $._currentUrl = url
-    this.action($)
+      .then(res => res.text);
+    $ = cheerio.load(html);
+    $._currentUrl = url;
+    this.action($);
   }
 
   // postInit
   if (this.postInit && isPromise(p = this.postInit())) {
-    yield p
+    yield p;
   }
-})
+});
--- test/simple.js
+++ test/simple.js
@@ -1,108 +1,108 @@
-'use strict'
+'use strict';
 
 /**
  * module dependencies
  */
 
-const resolve = require('url').resolve
-const should = require('should')
-const _ = require('lodash')
-const co = require('co')
-const sleep = require('promise.delay')
-const NextPage = require('../')
+const resolve = require('url').resolve;
+const should = require('should');
+const _ = require('lodash');
+const co = require('co');
+const sleep = require('promise.delay');
+const NextPage = require('../');
 
 describe('Simple', function() {
 
   it('it works', function*() {
     const n = new NextPage({
       init() {
-        this.topics = []
+        this.topics = [];
       },
 
       postInit() {
-        this.topics = _.flatten(this.topics)
+        this.topics = _.flatten(this.topics);
       },
 
       action($) {
         const topics = $('#TopicsNode .cell').map((_, el) => {
-          const $el = $(el)
+          const $el = $(el);
 
           // url
-          let url = $el.find('.item_title a').attr('href')
-          url = resolve($._currentUrl, url)
+          let url = $el.find('.item_title a').attr('href');
+          url = resolve($._currentUrl, url);
 
           return {
             title: $el.find('.item_title').text(),
             author: $el.find('.item_title').next().next().find('strong').text(),
             url: url
-          }
-        }).get()
+          };
+        }).get();
 
-        this.topics.push(topics)
+        this.topics.push(topics);
       },
 
       hasNext($) {
-        return true
+        return true;
       },
 
       getNext($) {
-        return $('.page_current').eq(0).next().attr('href')
+        return $('.page_current').eq(0).next().attr('href');
       }
-    })
+    });
 
     yield n.run('http://v2ex.com/go/share', {
       limit: 3
-    })
+    });
 
-    n.topics.should.be.Array()
-    n.topics.length.should.equal(20 * 3)
-  })
+    n.topics.should.be.Array();
+    n.topics.length.should.equal(20 * 3);
+  });
 
   it('async init/postInit', function*() {
     const n = new NextPage({
       init: co.wrap(function*() {
-        yield sleep(10)
-        this.x = 'x'
+        yield sleep(10);
+        this.x = 'x';
       }),
 
       postInit: co.wrap(function*() {
-        this.y = 'y'
+        this.y = 'y';
       }),
 
       action($) {
-        this.title = $('title').text()
+        this.title = $('title').text();
       },
       hasNext: $ => false,
       getNext: $ => undefined
-    })
+    });
 
-    yield n.run('http://www.qq.com/')
-    n.x.should.equal('x')
-    n.y.should.equal('y')
-    n.title.should.match(/腾讯/)
-  })
-})
+    yield n.run('http://www.qq.com/');
+    n.x.should.equal('x');
+    n.y.should.equal('y');
+    n.title.should.match(/腾讯/);
+  });
+});
 
 describe('Error', function() {
   it('when action/hasNext/getNext empty', function*() {
     (function() {
-      new NextPage()
-    }).should.throw(/action/).throw(/empty/)
-  })
+      new NextPage();
+    }).should.throw(/action/).throw(/empty/);
+  });
 
   it('when url empty in run', function*() {
-    const noop = $ => undefined
+    const noop = $ => undefined;
     const n = new NextPage({
       action: noop,
       hasNext: noop,
       getNext: noop
-    })
+    });
 
     try {
-      yield n.run()
+      yield n.run();
     } catch (e) {
-      e.should.be.Error()
-      e.message.should.match(/url/).match(/empty/)
+      e.should.be.Error();
+      e.message.should.match(/url/).match(/empty/);
     }
-  })
-})
+  });
+});
